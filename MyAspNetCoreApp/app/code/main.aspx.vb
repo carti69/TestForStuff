45d250e4d8cd0c3d4f74f72409ab5dcdac25da18
carti69 <zhukow28@gmail.com>
2024-08-23 15:43:34 -0600

    putting all the code for MyAspNetCoreApp

--- MyAspNetCoreApp/app/code/main.aspx.vb
+++ MyAspNetCoreApp/app/code/main.aspx.vb
@@ -1,12 +1,1 @@
-Imports System.IO
-Imports System.Net
-Imports System.Data
-Imports System.Data.OleDb
-Imports System.Configuration 
-Imports System.Data.SqlClient
-Imports System.Web.UI.Page
-Imports System.Web.UI.WebControls
-Imports System.IO.Compression
-Imports System.Web.Script.Serialization
-
-
+' test
